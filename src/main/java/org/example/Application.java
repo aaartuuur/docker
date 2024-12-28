f27669ac264fb4bb3c17aad44a9250504ea860f4
Artur <shkodartur@gmail.com>
2024-12-28 18:15:42 +0300

    last

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -6,12 +6,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.example"})
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(Application.class, args);
-
-        Runner runner = context.getBean(Runner.class);
-        runner.run(args);
+        SpringApplication.run(Application.class, args);
     }
+
 }
